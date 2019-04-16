6cf9631c1f378e6531d34bad3170986a7f095def
Devin <dmmagee87@yahoo.com>
2019-04-15 23:18:38 -0700

    added list array of brands, added beerexpert class

--- app/src/main/java/edu/osucascades/beeradviser/FindBeerActivity.java
+++ app/src/main/java/edu/osucascades/beeradviser/FindBeerActivity.java
@@ -1,12 +1,14 @@
 package edu.osucascades.beeradviser;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
+import java.util.List;
 
 public class FindBeerActivity extends Activity {
+    private BeerExpert expert = new BeerExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +16,7 @@ public class FindBeerActivity extends Activity {
         setContentView(R.layout.activity_find_beer);
     }
 
-    // Called when the button gets clicked
+    //Call when the button gets clicked
     public void onClickFindBeer(View view) {
         //Get a reference to the TextView
         TextView brands = (TextView) findViewById(R.id.brands);
@@ -22,7 +24,13 @@ public class FindBeerActivity extends Activity {
         Spinner color = (Spinner) findViewById(R.id.color);
         //Get the selected item in the Spinner
         String beerType = String.valueOf(color.getSelectedItem());
-        //Display the selected item
-        brands.setText(beerType);
+        //Get recommendations from the BeerExpert class
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand : brandsList) {
+            brandsFormatted.append(brand).append('\n');
+        }
+        //Display the beers
+        brands.setText(brandsFormatted);
     }
 }
