8f22427b766f41f01d3f6819dae5e437d50747fa
Devin <dmmagee87@yahoo.com>
2019-04-15 23:02:44 -0700

    added objects to findbeeractivity for version 1

--- app/src/main/java/edu/osucascades/beeradviser/FindBeerActivity.java
+++ app/src/main/java/edu/osucascades/beeradviser/FindBeerActivity.java
@@ -1,4 +1,28 @@
 package edu.osucascades.beeradviser;
 
-public class FindBeerActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class FindBeerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+
+    // Called when the button gets clicked
+    public void onClickFindBeer(View view) {
+        //Get a reference to the TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+        //Get a reference to the Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+        //Get the selected item in the Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+        //Display the selected item
+        brands.setText(beerType);
+    }
 }
